8cee6445956128c148d5596701f2ba381be777b5
yelo-o <flyordig@gmail.com>
2024-01-24 22:42:10 +0900

    callback -> promise 변경완료

--- DreamCoding/async/callback-to-promise.js
+++ DreamCoding/async/callback-to-promise.js
@@ -30,47 +30,39 @@ printWithDelay(function() {
 
 // Callback Hell example
 class UserStorage {
-  loginUser(id, password, onSuccess, onError) {
-    setTimeout(function() {
-      if (
-        (id === 'ellie' && password === 'dream') ||
-        (id === 'coder' && password === 'academy')
-      ) {
-        onSuccess(id);
-      } else {
-        onError(new Error('not found'));
-      }
-    }, 2000);
-  }
+    loginUser(id, password) {
+        return new Promise((resolve, reject) => {
+            setTimeout(function() {
+                if (
+                    (id === 'ellie' && password === 'dream') ||
+                    (id === 'coder' && password === 'academy')
+                ) {
+                    resolve(id);
+                } else {
+                    reject(new Error('not found'));
+                }
+            }, 2000);
+        });
+    }
 
-  getRoles(user, onSuccess, onError) {
-    setTimeout(function() {
-      if (user === 'ellie') {
-        onSuccess({name: 'ellie', role: 'admin'});
-      } else {
-        onError(new Error('no access'));
+    getRoles(user, onSuccess, onError) {
+        return new Promise ((resolve, reject) => {
+            setTimeout(function() {
+                if (user === 'ellie') {
+                    resolve({name: 'ellie', role: 'admin'});
+                } else {
+                    reject(new Error('no access'));
+                }
+            }, 1000);
+        });
       }
-    }, 1000);
-  }
 }
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
-userStorage.loginUser(
-  id,
-  password,
-  function(user) {
-    userStorage.getRoles(
-      user,
-      function(userWithRole) {
-        alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
-      },
-      function(error) {
-        console.log(error);
-      })
-  },
-  function(error) {
-    console.log(error);
-  }
-)
+
+userStorage.loginUser(id, password)
+    .then(userStorage.getRoles)
+    .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
+    .catch(console.log);

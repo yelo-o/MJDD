6ff99ef333df6da9d6a5e96778f6b3977cc18631
yelo-o <flyordig@gmail.com>
2024-01-26 22:40:59 +0900

    Promise 내부는 동기적으로 처리된다. 다만 결과값은 비동기적으로 처리된다.

--- ZeroCho/EngineStudy/2.2_async/async.js
+++ ZeroCho/EngineStudy/2.2_async/async.js
@@ -1,14 +1,22 @@
-setTimeout(() => {
-    console.log('a');
-}, 0);
+let a = 2;
 
-setTimeout(() => {
-    console.log('b');
-}, 1000);
+const p = new Promise((resolve, reject) => {
+    console.log('제일 먼저');
+    setTimeout(() => {
+        a = 5;
+        console.log(a);
+        resolve(a);
+    }, 0);
+});
 
-setTimeout(() => {
-    console.log('c');
-}, 2000);
+// 딴짓딴짓
+console.log('딴짓');
+// 딴짓딴짓
+p.then((result) => {
+    console.log('result', result);
+});
 
 // 한 번 비동기는 영원한 비동기
 // 비동기는 동시의 문제가 아니다. 순서의 문제다.
+// Promise 란, 실행은 바로 하되, 결괏값은 원하는 시점에 쓸 수 있는 것
+// promise, process.nextTick -> micro, 나머지는 매크로
